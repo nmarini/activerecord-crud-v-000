8ef788a7fd0f1079195627be8daed641167c2224
Nicolas Marini <nmarini44@gmail.com>
2018-09-21 21:58:54 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,9 +23,9 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.new.tap do |i|
     attributes.each do |key, value|
       self.send("#{key}=", value)
-    end 
+    end
     self.save
-  end 
+  end
 end
 
 def can_be_created_in_a_block(args = __)
