4c7026d4c189b02927bb2268b9b4867383f1910e
Nicolas Marini <nmarini44@gmail.com>
2018-09-21 22:48:46 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -54,8 +54,9 @@ end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
-  Movie.create(title: "Awesome Flick")
-  __
+  movei = Movie.create(title: "Awesome Flick")
+  movie.update(title: "Even Awesomer Flick")
+  movie.save
   __
   __
 end
